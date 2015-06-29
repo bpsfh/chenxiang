ed402023f783a93cae0a7fc6329390566810d5c6
bpsfh <hui-fan@rontech.co.jp>
2015-06-29 18:59:58 +0800

    Add the module of Salesman

---
+++ salesman/controller/common/profile.php
@@ -0,0 +1,31 @@
+<?php
+class ControllerCommonProfile extends Controller {
+	public function index() {
+		$this->load->language('common/menu');
+
+		$this->load->model('salesman/user');
+		$this->load->model('tool/image');
+
+		$user_info = $this->model_salesman_user->getSalesmanByEmail($this->salesman->getEmail());
+
+		if ($user_info) {
+			$data['fullname'] = $user_info['fullname'];
+			$data['email'] = $user_info['email'];
+//			$data['user_group'] = $user_info['user_group'] ;
+
+			/*
+			if (is_file(DIR_IMAGE . $user_info['image'])) {
+				$data['image'] = $this->model_tool_image->resize($user_info['image'], 45, 45);
+			} else {
+				$data['image'] = $this->model_tool_image->resize('no_image.png', 45, 45);
+			}
+			*/
+			$data['image'] = $this->model_tool_image->resize('no_image.png', 45, 45);
+		} else {
+			$data['email'] = '';
+//			$data['image'] = '';
+		}
+
+		return $this->load->view('common/profile.tpl', $data);
+	}
+}
---
+++ salesman/language/zh-CN/common/menu.php
@@ -0,0 +1,18 @@
+<?php
+
+$_['text_dashboard']                   = '管理首页';
+$_['text_vip_card_mgmt']               = 'VIP卡管理';
+$_['text_vip_card_srch']               = 'VIP卡查询';
+$_['text_vip_record_srch']             = '消费记录查询';
+$_['text_vip_card_apply']              = 'VIP卡申请';
+$_['text_financial']                   = '财务管理';
+$_['text_commission_srch']             = '佣金查询';
+$_['text_settle_srch']                 = '结算查询';
+$_['text_settle_account']              = '我要结算';
+$_['text_account']                     = '帐户管理';
+$_['text_system_notice']               = '系统公告';
+$_['text_basic_info']                  = '基本信息设置';
+$_['text_bank_info']                   = '银行信息设置';
+$_['text_customer_group']              = '会员分组管理';
+$_['text_contact_us']                  = '联系我们';
+$_['text_invoice_upload']              = '发票上传';
