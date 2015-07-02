0b736fb45c96baa60983fd09901d28bf9bf0a8cf
jie-zhang <jie-zhang@rontech.co.jp>
2015-07-02 10:24:12 +0800

    Modified the install files and sql files for installation.

--- install/cli_install.php
+++ install/cli_install.php
@@ -261,6 +261,49 @@ function write_config_files($options) {
 	fwrite($file, $output);
 
 	fclose($file);
+
+	$output  = '<?php' . "\n";
+	$output .= '// HTTP' . "\n";
+	$output .= 'define(\'HTTP_SERVER\', \'' . $options['http_server'] . 'salesman/\');' . "\n";
+	$output .= 'define(\'HTTP_ADMIN\', \'' . $options['http_server'] . 'admin/\');' . "\n";
+	$output .= 'define(\'HTTP_CATALOG\', \'' . $options['http_server'] . '\');' . "\n";
+	$output .= 'define(\'HTTP_IMAGE\', \'' . $options['http_server'] . 'image/\');' . "\n\n";
+
+	$output .= '// HTTPS' . "\n";
+	$output .= 'define(\'HTTPS_SERVER\', \'' . $options['http_server'] . 'salesman/\');' . "\n";
+	$output .= 'define(\'HTTPS_ADMIN\', \'' . $options['http_server'] . 'admin/\');' . "\n";
+	$output .= 'define(\'HTTPS_CATALOG\', \'' . $options['http_server'] . '\');' . "\n";
+	$output .= 'define(\'HTTPS_IMAGE\', \'' . $options['http_server'] . 'image/\');' . "\n\n";
+
+	$output .= '// DIR' . "\n";
+	$output .= 'define(\'DIR_APPLICATION\', \'' . DIR_MYCNCART . 'salesman/\');' . "\n";
+	$output .= 'define(\'DIR_SYSTEM\', \'' . DIR_MYCNCART . 'system/\');' . "\n";
+	$output .= 'define(\'DIR_DATABASE\', \'' . DIR_MYCNCART . 'system/database/\');' . "\n";
+	$output .= 'define(\'DIR_LANGUAGE\', \'' . DIR_MYCNCART . 'salesman/language/\');' . "\n";
+	$output .= 'define(\'DIR_TEMPLATE\', \'' . DIR_MYCNCART . 'salesman/view/template/\');' . "\n";
+	$output .= 'define(\'DIR_CONFIG\', \'' . DIR_MYCNCART . 'system/config/\');' . "\n";
+	$output .= 'define(\'DIR_IMAGE\', \'' . DIR_MYCNCART . 'image/\');' . "\n";
+	$output .= 'define(\'DIR_CACHE\', \'' . DIR_MYCNCART . 'system/cache/\');' . "\n";
+	$output .= 'define(\'DIR_DOWNLOAD\', \'' . DIR_MYCNCART . 'system/download/\');' . "\n";
+	$output .= 'define(\'DIR_UPLOAD\', \'' . DIR_MYCNCART . 'system/upload/\');' . "\n";
+	$output .= 'define(\'DIR_LOGS\', \'' . DIR_MYCNCART . 'system/logs/\');' . "\n";
+	$output .= 'define(\'DIR_MODIFICATION\', \'' . DIR_MYCNCART . 'system/modification/\');' . "\n";
+	$output .= 'define(\'DIR_CATALOG\', \'' . DIR_MYCNCART . 'catalog/\');' . "\n\n";
+
+	$output .= '// DB' . "\n";
+	$output .= 'define(\'DB_DRIVER\', \'' . addslashes($options['db_driver']) . '\');' . "\n";
+	$output .= 'define(\'DB_HOSTNAME\', \'' . addslashes($options['db_hostname']) . '\');' . "\n";
+	$output .= 'define(\'DB_USERNAME\', \'' . addslashes($options['db_username']) . '\');' . "\n";
+	$output .= 'define(\'DB_PASSWORD\', \'' . addslashes($options['db_password']) . '\');' . "\n";
+	$output .= 'define(\'DB_DATABASE\', \'' . addslashes($options['db_database']) . '\');' . "\n";
+	$output .= 'define(\'DB_PREFIX\', \'' . addslashes($options['db_prefix']) . '\');' . "\n";
+	$output .= '?>';
+
+	$file = fopen(DIR_MYCNCART . 'salesman/config.php', 'w');
+
+	fwrite($file, $output);
+
+	fclose($file);
 }
 
 
@@ -296,6 +339,7 @@ case "install":
 		echo "成功! MyCnCart 成功安装于您的服务器\n";
 		echo "网站前台页面链接: " . $options['http_server'] . "\n";
 		echo "网站后台管理链接: " . $options['http_server'] . "admin/\n\n";
+		echo "网站业务员管理链接: " . $options['http_server'] . "salesman/\n\n";
 	} catch (ErrorException $e) {
 		echo '失败!: ' . $e->getMessage() . "\n";
 		exit(1);
