af94e1cf796297a4e9ded6399cbda16bb5f3564b
sangsang-hu <sangsang-hu@rontech.co.jp>
2015-09-09 16:22:45 +0800

    Add the function of sub_salesman's vip card.
    1. Modify the old function of salesman's vip card.
    2. Add the function of show vip card applications.
    3. Add the function of approve vip card application.
    4. Add the function of rejust vip card application.

--- admin/model/salesman/vip_card.php
+++ admin/model/salesman/vip_card.php
@@ -13,6 +13,18 @@ class ModelSalesmanVipCard extends Model {
 		$this->db->query("INSERT INTO " . DB_PREFIX . "vip_card SET vip_card_num = '" . $this->db->escape($data['vip_card_num'])
 				 . "', salesman_id = '" . (int)$data['salesman_id'] . "', bind_status = 1" . ", date_bind_to_salesman = NOW()"
 				 . ", date_created = NOW()" . ", date_modified = NOW()");
+		
+		$this->addVipCardAssignRecord($data);
+	}
+	
+	
+	/**
+	 * 插入VIP卡分配记录表
+	 */
+	public function addVipCardAssignRecord($data) {
+		
+		$this->db->query("INSERT INTO " . DB_PREFIX . "vip_card_assign_record SET vip_card_num = '" . $this->db->escape($data['vip_card_num'])
+				. "', salesman_id = '" . (int)$data['salesman_id'] . "', date_created = NOW()" . ", date_modified = NOW(), is_valid = true");
 	}
 
 }
