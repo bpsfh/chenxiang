5c2d9a197c87169654f9b5dde5f375571d788b08
jie-zhang <jie-zhang@rontech.co.jp>
2015-09-21 12:44:46 +0800

    Modify the copyright declaration.

--- salesman/language/zh-CN/common/footer.php
+++ salesman/language/zh-CN/common/footer.php
@@ -3,5 +3,5 @@
  * $Author: http://www.opencartchina.com 
 **/
 // Text
-$_['text_footer'] 	= '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_footer'] 	= '<a href="http://www.shenhenet.com">神鹤网</a> &copy; 2015-' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] 	= '版本号 %s';
